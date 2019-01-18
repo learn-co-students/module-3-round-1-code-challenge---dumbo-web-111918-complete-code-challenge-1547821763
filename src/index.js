92cbfeb6e50b3fd5c34edb698f13710ae5902bdb
Joseph <joeephus@gmail.com>
2019-01-18 10:31:37 -0500

    let's see

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
-
+  let likesBtn = document.querySelector("button")
+  let imageDiv = document.getElementById("image_card");
+  let imageName = document.getElementById("name");
+  let commentForm = document.getElementById("comment_form")
+  // console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  
+  let imageId = 1857 //Enter the id from the fetched image here
+  
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
+  
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
+  
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  
+  //FUNCTIONS FOR CHANGING AND UPDATING LIKES
+  likesBtn.addEventListener("click", function(){
+    event.preventDefault();
+    console.log("working")
+  })
 
+  //COMMENT FORM FUNCTIONALITY
+  commentForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    let formValue = document.getElementById("comment_input");
+    formObj = {
+      image_id: imageId,
+      content: formValue.value
+    }
+    renderNewComment(formObj);
+    fetch(commentsURL, {
+      method: "POST", 
+      headers: {
+        'Accept': 'application/json',
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify(formObj), 
+    })
+    .then(response => response.json())
+  })
+  
+  function renderNewComment(obj) {
+    let commentSection = document.getElementById("comments");
+    commentSection.innerHTML += 
+    `<li data-id="${formObj.id}"> ${formObj.content}</li>`
+  }
+  
+  //FUNCTIONS FOR RENDERING IMAGE AND CHANGING IMAGE TITLE
+  fetch(imageURL)
+  .then(obj => obj.json())
+  .then(function(parsedObj) {
+    imageName.innerText = `${parsedObj.name}`
+    appendImage(parsedObj)
+  })
+  
+  function appendImage(obj) {
+    imageDiv.innerHTML += 
+    `<img src="http://blog.flatironschool.com/wp-content/uploads/2017/06/IMAG2936-352x200.jpg"/>`
+  }
+  
 })
+  
